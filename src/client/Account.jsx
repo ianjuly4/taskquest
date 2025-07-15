09a1ee424326f1ce60bf9abed22b08b0efbfa147
ianjuly4 <ianjuly4@gmail.com>
2025-07-14 22:34:06 -0600

    create project, git init, tailwind install

---
+++ src/client/Account.jsx
@@ -0,0 +1,12 @@
+// Account.jsx
+import React from 'react';
+
+const Account = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Account;
---
+++ src/client/TaskContainer.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import TaskCard from "../TaskCard.jsx"
+
+const TaskContainer = () => {
+
+    return(
+        <div>
+            <TaskCard/>
+        </div>
+    )
+}
